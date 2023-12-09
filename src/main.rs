8152124ac4fe0314538482bbac3757999f0c8e55
Prabhat1308 <prabhatverma329@gmail.com>
2023-12-09 16:59:51 +0530

    Branch updated

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,7 @@
 
 #![cfg_attr(not(feature = "export-abi"), no_main)]
 
-// #[cfg(feature = "export-abi")]
-
-// fn main() {
-//     stylus_hello_world::main();
-// }
-use alloc::{string::String, vec::Vec};
-use stylus_sdk::{alloy_primitives::U256, call, msg, prelude::*};
-
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-
-
-
+#[cfg(feature = "export-abi")]
+fn main() {
+    stylus_hello_world::main();
+}
